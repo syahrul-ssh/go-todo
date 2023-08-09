d0b422ef84c95369eb9c22bdc2ea8afc0f930dfd
syahrul safarudin hasan <syahrul.safarudinh9@gmail.com>
2023-08-09 10:57:06 +0700

    implementing fiber to listen HTTP request

---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"log"
+
+	"github.com/gofiber/fiber/v2/middleware/cors"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+func main() {
+	app := fiber.New()
+	app.Use(cors.New())
+
+	api := app.Group("/api")
+
+	// Test handler
+	api.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("App is Running!")
+	})
+
+	log.Fatal(app.Listen(":5000"))
+}
